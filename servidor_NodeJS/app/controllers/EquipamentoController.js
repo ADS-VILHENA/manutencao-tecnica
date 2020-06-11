90547784510cbc8521faa2e6219aa45b5e4e0f35
julianomellow70 <julianodemello1999@gmail.com>
2020-06-11 16:33:30 -0500

    Cadastrando equipamentos

--- servidor_NodeJS/app/controllers/EquipamentoController.js
+++ servidor_NodeJS/app/controllers/EquipamentoController.js
@@ -9,5 +9,57 @@ module.exports.visualizar_listar_equipamentos = (aplicacao, req, res)=>{
         let equipamentos = result;
         res.render("painel admin/pesquisar_equipamento",{equipamentos}) ;
     })
- 
 }
+
+module.exports.visualizar_cadastrar_equipamentos = (aplicacao, req, res)=>{
+  let erros = {};
+  res.render("painel admin/cadastrar_equipamento",{erros});
+}
+module.exports.visualizar_atualizar_equipamentos = (aplicacao, req, res,id_equipamento)=>{
+  let erros = {};
+  let id;
+  res.render("painel admin/atualizar_equipamento",{erros});
+}
+
+module.exports.atualizar_equipamento = (aplicacao, req, res)=>{
+  
+  let equipamento = {
+    nome_equipamento:req.body.nome_equipamento,
+    descricao_equipamento:req.body.descricao_equipamento,
+    id_equipamento:req.body.id_equipamento
+  }
+  
+  var conexao = aplicacao.config.db;
+  var equipamentoDAO = new aplicacao.app.model.EquipamentoDAO(conexao);
+  
+  equipamentoDAO.atualizar_equipamento(equipamento, (requisicao, result) => {
+     // console.log(result);
+
+  //parametros para renderizar atualizar equipamento
+   
+    let erros = {};
+    let dados_equipamento = {nome_equipamento:req.body.nome_equipamento,descricao_equipamento:req.body.descricao_equipamento};
+    let id_equipamento = {id_equipamento:req.body.id_equipamento};
+   
+   res.render("painel admin/atualizar_equipamento",{erros,id_equipamento,dados_equipamento});
+});
+
+}
+
+
+module.exports.salvar_equipamento = function(aplicacao,req,res){
+  var conexao = aplicacao.config.db;
+  var equipamento_model = new aplicacao.app.model.EquipamentoDAO(conexao);
+  var equipamento = req.body;
+  
+  equipamento_model.salvar_equipamento(equipamento, (requisicao, result) => {
+    let id_inserido =  result.insertId;
+    let erros = {};
+    let dados_equipamento = {nome_equipamento:req.body.nome_equipamento,descricao_equipamento:req.body.descricao_equipamento};
+    let id_equipamento = {id_equipamento:id_inserido}
+    
+    res.render("painel admin/atualizar_equipamento",{erros,id_equipamento,dados_equipamento});
+      
+  });
+}
+
--- servidor_NodeJS/app/routes/painel_admin.js
+++ servidor_NodeJS/app/routes/painel_admin.js
@@ -5,4 +5,20 @@ aplicacao.get('/painel_admin/listar_equipamentos', (req, res) => {
         aplicacao.app.controllers.EquipamentoController.visualizar_listar_equipamentos(aplicacao, req, res);
     //aplicacao.app.controllers.ChamadoController.visualizar_home(aplicacao, req, res, { erro: 0 },erros_submit);
 })
+
+aplicacao.get('/painel_admin/cadastrar_equipamentos', (req, res) => {
+    
+     aplicacao.app.controllers.EquipamentoController.visualizar_cadastrar_equipamentos(aplicacao, req, res);
+ 
+})
+
+aplicacao.post('/painel_admin/acao/salvar_equipamento', (req, res) => {
+   
+    aplicacao.app.controllers.EquipamentoController.salvar_equipamento(aplicacao, req, res);
+})
+
+aplicacao.post('/painel_admin/acao/atualizar_equipamento',(req,res)=>{
+    aplicacao.app.controllers.EquipamentoController.atualizar_equipamento(aplicacao, req, res);
+})
+
 }
