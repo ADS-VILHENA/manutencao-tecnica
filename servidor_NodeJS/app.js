5d5fdb0f1938709478c01edb532fbb6eb50530ee
Juliano de Mello <julianodemello1999@gmail.com>
2020-06-28 14:12:31 -0400

    Corrigindo o bug da camera no host

--- servidor_NodeJS/app.js
+++ servidor_NodeJS/app.js
@@ -1,8 +1,21 @@
 //importar as configurações do servidor
+const https = require("https"),
+fs = require("fs");
 
+//importando os certificados
+const options = {
+    key: fs.readFileSync("config/certificados/key.pem"),
+    cert: fs.readFileSync("config/certificados/cert.pem")
+  };
 var app = require('./config/server');
-
-//parametrizar nossa porta de escuta
+/*
+//executando com http
 app.listen(2525,()=>{
     console.log('Servidor online na porta 2525!');
 })
+*/
+
+//executando com https
+https.createServer(options, app).listen(2525,()=>{
+    console.log("Servidor online em : https://localhost:2525/")
+});
