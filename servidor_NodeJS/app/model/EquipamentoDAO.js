5f0dd4074213e1cb0e4eea5542c4a8811835fbb6
Juliano de Mello <julianodemello1999@gmail.com>
2020-06-29 23:33:18 -0400

    CRUD equipamentos

--- servidor_NodeJS/app/model/EquipamentoDAO.js
+++ servidor_NodeJS/app/model/EquipamentoDAO.js
@@ -3,9 +3,26 @@ function EquipamentoDAO(conexao) {
 }
 
 
-EquipamentoDAO.prototype.buscar_equipamentos_bd = function(callback){
-   this._conexao.query('SELECT * FROM equipamento;',callback); 
+EquipamentoDAO.prototype.buscar_equipamentos_bd = function(termo,callback){
+   this._conexao.query("SELECT * FROM equipamento where nome_equipamento like '%"+termo+"%';",callback); 
 }
+
+EquipamentoDAO.prototype.buscar_equipamento_bd_by_id = function(id,callback){
+    this._conexao.query('SELECT * FROM equipamento where id_equipamento ='+id+';',callback); 
+ }
+ 
+ EquipamentoDAO.prototype.deletar_problema_bd_by_id = function(id,callback){
+    this._conexao.query('DELETE FROM equipamento_problema where id_equipamento_problema ='+id+';',callback); 
+ }
+
+ EquipamentoDAO.prototype.deletar_equipamento_bd_by_id = function(id,callback){
+    this._conexao.query('DELETE FROM equipamento where id_equipamento ='+id+';',callback); 
+ }
+
+ EquipamentoDAO.prototype.adicionar_problema = function(id_equipamento,nome_problema,callback){
+    this._conexao.query('insert into equipamento_problema(id_equipamento, nome_problema) values ('+id_equipamento+',"'+nome_problema+'");',callback); 
+ }
+ 
 
 EquipamentoDAO.prototype.salvar_equipamento = function(equipamento,callback){
     this._conexao.query('INSERT INTO equipamento SET ?', equipamento, callback);
@@ -14,6 +31,10 @@ EquipamentoDAO.prototype.salvar_equipamento = function(equipamento,callback){
    let query = "UPDATE equipamento SET nome_equipamento = '"+equipamento.nome_equipamento+"', descricao_equipamento = '"+equipamento.descricao_equipamento+"' WHERE id_equipamento ="+equipamento.id_equipamento+";";
    this._conexao.query(query, callback);
  }
+ EquipamentoDAO.prototype.buscar_problemas = function(id,callback){
+    let query = "select * from equipamento_problema where id_equipamento ="+id;
+    this._conexao.query(query, callback);
+  }
 
 module.exports = () => {
     return EquipamentoDAO;
--- servidor_NodeJS/app/routes/painel_admin.js
+++ servidor_NodeJS/app/routes/painel_admin.js
@@ -1,9 +1,16 @@
 module.exports = (aplicacao)=>{
 //rota para pesquisar equipamentos no painel admin
-aplicacao.get('/painel_admin/listar_equipamentos', (req, res) => {
+aplicacao.get('/painel_admin/pesquisar_equipamentos', (req, res) => {
+        let termo = req.query.termo;
+        if(termo == undefined){
+           termo = "";
+        }
        // res.send("Listar equipamentos");
-        aplicacao.app.controllers.EquipamentoController.visualizar_listar_equipamentos(aplicacao, req, res);
+        aplicacao.app.controllers.EquipamentoController.visualizar_listar_equipamentos(aplicacao, req, res,termo);
     //aplicacao.app.controllers.ChamadoController.visualizar_home(aplicacao, req, res, { erro: 0 },erros_submit);
+
+
+
 })
 
 aplicacao.get('/painel_admin/cadastrar_equipamentos', (req, res) => {
@@ -20,17 +27,44 @@ aplicacao.post('/painel_admin/acao/salvar_equipamento', (req, res) => {
 aplicacao.post('/painel_admin/acao/atualizar_equipamento',(req,res)=>{
     aplicacao.app.controllers.EquipamentoController.atualizar_equipamento(aplicacao, req, res);
 })
-//rota para listar tipos de area painel admin
-aplicacao.get('/painel_admin/pesquisar_tipo_areas', (req, res) => {
-    aplicacao.app.controllers.TipoAreaController.visualizar_listar_tipo_area(aplicacao, req, res);
+aplicacao.get('/painel_admin/atualizar_equipamento_by_id/:id',(req,res)=>{
+    let id = req.params.id;
+    console.log(id);
+    aplicacao.app.controllers.EquipamentoController.atualizar_equipamento_by_id(aplicacao, req, res, id);
 })
 
-//rota para listar area painel admin
+//rotas para problemas de equipamento
+aplicacao.get('/painel_admin/deletar_equipamento_by_id',(req,res)=>{
+    let id_equipamento = req.query.id_equipamento;
+    aplicacao.app.controllers.EquipamentoController.deletar_equipamento_by_id(aplicacao, req, res,id_equipamento);
+})
+aplicacao.get('/painel_admin/deletar_problema_by_id',(req,res)=>{
+    let id_problema = req.query.id_problema;
+    let id_equipamento = req.query.id_equipamento;
+    
+   aplicacao.app.controllers.EquipamentoController.deletar_problema_by_id(aplicacao, req, res, id_problema, id_equipamento);
+})
+aplicacao.get('/painel_admin/adcionar_problema_by_id',(req,res)=>{
+
+    let id_equipamento = req.query.id_equipamento;
+    let nome_problema = req.query.nome_problema;
+    
+   aplicacao.app.controllers.EquipamentoController.adicionar_equipamento_by_id(aplicacao, req, res, id_equipamento,nome_problema);
+})
+
+//---------------------------------------------------
+
+//rotas para area
 aplicacao.get('/painel_admin/pesquisar_areas', (req, res) => {
    aplicacao.app.controllers.AreaController.visualizar_listar_area(aplicacao, req, res);
 })
 
+//---------------------------------------------------
 // rotas tipo_area
+//rota para listar tipos de area painel admin
+aplicacao.get('/painel_admin/pesquisar_tipo_areas', (req, res) => {
+    aplicacao.app.controllers.TipoAreaController.visualizar_listar_tipo_area(aplicacao, req, res);
+})
 aplicacao.get('/painel_admin/cadastrar_tipoarea', (req, res) => {
     
     aplicacao.app.controllers.TipoAreaController.visualizar_cadastrar_tipoarea(aplicacao, req, res);
@@ -46,8 +80,6 @@ aplicacao.post('/painel_admin/acao/salvar_tipoarea', (req, res) => {
   
    aplicacao.app.controllers.TipoAreaController.salvar_tipoarea(aplicacao, req, res);
 })
-
-
 
 aplicacao.post('/painel_admin/acao/atualizar_tipoarea',(req,res)=>{
    aplicacao.app.controllers.TipoAreaController.atualizar_tipoarea(aplicacao, req, res);
