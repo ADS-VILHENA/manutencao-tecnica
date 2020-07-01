29423f2c7c4d2cbe9928ea6280d3cce308a7383a
Juliano de Mello <julianodemello1999@gmail.com>
2020-06-30 23:40:05 -0400

    CRUD AREA

--- servidor_NodeJS/app/model/AreaDAO.js
+++ servidor_NodeJS/app/model/AreaDAO.js
@@ -11,6 +11,39 @@ AreaDAO.prototype.atualizar_area = function(area,callback){
     this._conexao.query(query, callback);
   }
 
+  AreaDAO.prototype.salvar_area = function(area,callback){
+    this._conexao.query('INSERT INTO area SET ?', area, callback);
+ }
+
+ AreaDAO.prototype.buscar_areas_bd = function(termo,callback){
+    this._conexao.query("SELECT * FROM area where nome like '%"+termo+"%';",callback); 
+ }
+ AreaDAO.prototype.deletar_area_bd_by_id = function(id,callback){
+    let query = 'DELETE FROM area where id_area ='+id+';';
+    console.log(query);
+    this._conexao.query(query,callback); 
+ }
+ AreaDAO.prototype.buscar_area_bd_by_id = function(id,callback){
+    this._conexao.query('SELECT * FROM area where id_area ='+id+';',callback); 
+ }
+ 
+ AreaDAO.prototype.buscar_area_by_area_id = function(id,callback){
+     let query = 'SELECT e.nome_equipamento, ae.id_area_equipamento FROM equipamento AS e INNER JOIN area_equipamento AS ae on ae.id_equipamento  = e.id_equipamento INNER JOIN area AS a on a.id_area = ae.id_area where ae.id_area ='+id; 
+     //console.log(query);
+    this._conexao.query(query,callback); 
+ }
+ 
+ AreaDAO.prototype.adicionar_equipamento = function(id_equipamento,id_area,callback){
+    let query = 'INSERT INTO area_equipamento (id_area,id_equipamento) VALUES ('+id_area+','+id_equipamento+');'; 
+    //console.log(query);
+   this._conexao.query(query,callback); 
+}
+
+AreaDAO.prototype.deletar_area_equipamento = function(id_area_equipamento,callback){
+    let query = 'DELETE FROM area_equipamento where id_area_equipamento ='+id_area_equipamento; 
+    //console.log(query);
+   this._conexao.query(query,callback); 
+}
 module.exports = () => {
     return AreaDAO;
 }
--- servidor_NodeJS/app/routes/painel_admin.js
+++ servidor_NodeJS/app/routes/painel_admin.js
@@ -47,17 +47,70 @@ aplicacao.get('/painel_admin/deletar_problema_by_id',(req,res)=>{
 aplicacao.get('/painel_admin/adcionar_problema_by_id',(req,res)=>{
 
     let id_equipamento = req.query.id_equipamento;
-    let nome_problema = req.query.nome_problema;
+    let id_area = req.query.id_area;
     
-   aplicacao.app.controllers.EquipamentoController.adicionar_equipamento_by_id(aplicacao, req, res, id_equipamento,nome_problema);
+   aplicacao.app.controllers.AreaController.adicionar_equipamento_na_area(aplicacao, req, res, id_equipamento,id_area);
 })
 
 //---------------------------------------------------
 
 //rotas para area
+/*
 aplicacao.get('/painel_admin/pesquisar_areas', (req, res) => {
    aplicacao.app.controllers.AreaController.visualizar_listar_area(aplicacao, req, res);
 })
+*/
+//visualizar cadastrar area
+aplicacao.get('/painel_admin/cadastrar_area',(req,res)=>{
+aplicacao.app.controllers.AreaController.visualizar_cadastrar_area(aplicacao,req,res);
+})
+//salvar no bd e chamar a tela de atualizar area
+aplicacao.post('/painel_admin/acao/salvar_area', (req, res) => {
+    aplicacao.app.controllers.AreaController.salvar_area(aplicacao, req, res);
+})
+//pesquisar areas
+aplicacao.get('/painel_admin/pesquisar_areas', (req, res) => {
+    let termo = req.query.termo;
+    if(termo == undefined){
+       termo = "";
+    }
+    aplicacao.app.controllers.AreaController.visualizar_listar_areas(aplicacao, req, res,termo);
+})
+//rotas para problemas de equipamento
+aplicacao.get('/painel_admin/deletar_area_by_id',(req,res)=>{
+    let id_area = req.query.id_area;
+    aplicacao.app.controllers.AreaController.deletar_area_by_id(aplicacao, req, res,id_area);
+})
+aplicacao.get('/painel_admin/atualizar_area_by_id/:id',(req,res)=>{
+    let id = req.params.id;
+    let termo_busca_equipamento = req.query.termo_busca_equipamento;
+    //console.log(termo_busca_equipamento);
+    if(termo_busca_equipamento==undefined){
+        termo_busca_equipamento="dfasdfasdfasdfasgaefgsthtufyusrtywetWERQweQWD";
+    }
+    //console.log(id);
+    aplicacao.app.controllers.AreaController.atualizar_area_by_id(aplicacao, req, res, id,termo_busca_equipamento);
+})
+aplicacao.get('/painel_admin/adicionar_equipamento_na_area',(req,res)=>{
+    let id_equipamento = req.params.id_equipamento;
+    let id_area = req.query.id_area;
+    //console.log(termo_busca_equipamento);
+   
+    //console.log(id);
+    aplicacao.app.controllers.AreaController.adicionar_equipamento_na_area(aplicacao, req, res,id_equipamento,id_area);
+})
+aplicacao.get('/painel_admin/deletar_area_equipamento/:id',(req,res)=>{
+    let id_area_equipamento = req.query.id_area_equipamento;
+    let id_area = req.params.id;
+    //console.log(termo_busca_equipamento);
+   
+    //console.log(id);
+    aplicacao.app.controllers.AreaController.deletar_area_equipamento(aplicacao, req, res,id_area_equipamento,id_area);
+})
+
+
+
+
 
 //---------------------------------------------------
 // rotas tipo_area
