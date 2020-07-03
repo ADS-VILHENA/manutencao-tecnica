f4fd5c00430488d266605825cb4f7cc4efc46e96
jhonifigueredo <jhoni_choc@hotmail.com>
2020-07-03 02:21:58 -0400

    Corrigido botão, abreviação e rodapé da tela "atualizar_equipamento"

--- servidor_NodeJS/config/db.js
+++ servidor_NodeJS/config/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
         host:"localhost",
-        user:"root",
+        user:"hoot",
         password:"Suporte99",
         database: 'manutencao_tecnica'
     });
